66376bd28c314adc05f7f541539f1f1a3a9d210a
Rick Ahlgren <soul@Ricks-MBP.attlocal.net>
2020-06-07 16:55:52 -0400

    Completed problem 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,37 @@
-def has_negatives(a):
+def has_negatives(array):
     """
     YOUR CODE HERE
     """
-    # Your code here
 
-    return result
+    dictionary = {}
+    int_sum = 0
+
+    # Your code here
+    for element in array:
+        if element > 0:
+            for other_element in array:
+                if other_element < 0 and abs(other_element) == element:
+                    dictionary[element] = other_element
+                    print(f'{element}: {dictionary[element]}')
+
+
+                # int_sum = element + other_element
+                # if int_sum == 0:
+                #     dictionary[element] = other_element
+                #     print("This is dictionary: ", dictionary)
+        
+    final_array = []
+    for key in dictionary:
+        final_array.append(key)
+        
+    # print("This is final array: ", final_array)
+
+
+    return final_array
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    a = list(range(-1000, 5000, 5))
+    # print("This is a: ", a)
+    print(has_negatives(a))
+    # print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
